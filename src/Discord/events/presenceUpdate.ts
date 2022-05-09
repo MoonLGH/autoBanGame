0d97b274b22d5f774c08b5f760d739a2a5119b2a
MoonLGH <MoonLMC1212@gmail.com>
2022-05-09 05:30:20 +0000

    warning tfeature

--- src/Discord/events/presenceUpdate.ts
+++ src/Discord/events/presenceUpdate.ts
@@ -1,11 +1,25 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import {Presence} from "discord.js";
 import config from "../../utils/settings";
 export = {
 	name: "presenceUpdate",
 	async execute(PresenceOld:Presence,PresenceNew:Presence) {
 		for(const activity of PresenceNew.activities){
-			if(config.BannedGame.includes(activity.name)){
-				PresenceNew.member?.ban({reason:`Playing ${activity.name}`});
+			if(!PresenceNew.member) return;
+			if(!config.warningFirst && config.BannedGame.includes(activity.name)){
+				PresenceNew.member.ban({reason:`Playing ${activity.name}`});
+			}
+			if(config.warningFirst  && config.BannedGame.includes(activity.name)){
+				PresenceNew.member.send(`You've been warned for playing ${activity.name}\nPlease quickly close it or get banned on ${PresenceNew.member.guild.name} in 1 minute`);
+				setTimeout(()=>{
+					PresenceNew.member?.guild.members.fetch(PresenceNew.userId).then((member)=>{
+						for(const activity of member.presence!.activities){
+							if(config.BannedGame.includes(activity.name)){
+								member.presence!.member?.ban({reason:`Playing ${activity.name}`});
+							}
+						}
+					});
+				},1*1000*60);
 			}
 		}
 	},
--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,5 +1,6 @@
 export default {
 	intents: ["GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILDS", "GUILD_PRESENCES", "DIRECT_MESSAGES", "DIRECT_MESSAGE_REACTIONS", "GUILD_MEMBERS"],
 	partials: ["CHANNEL"],
-	BannedGame: ["Geometry Dash"]
+	BannedGame: ["Geometry Dash"],
+	warningFirst: true,
 };
